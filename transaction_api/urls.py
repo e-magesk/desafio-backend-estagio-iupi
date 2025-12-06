e19ca805b41e51c9ddecbfe9371229651e469e54
Eduarda <dudamagesk@gmail.com>
2025-12-05 21:56:22 -0300

    POST e GET ALL funcionando

--- transaction_api/urls.py
+++ transaction_api/urls.py
@@ -4,6 +4,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('transaction_api.urls'))
+    path('', views.transactions_manager)
 ]
--- transaction_api/views.py
+++ transaction_api/views.py
@@ -10,5 +10,28 @@ from .serializers import TransactionSerializer
 
 import json
 
-# @api_view(['GET'])
-# def get_all_transactions(response):
+@api_view(['POST', 'GET'])
+def transactions_manager(request):
+
+    # Criação da transição
+    if request.method == 'POST':
+
+        new_transaction = request.data
+        transaction_serializer = TransactionSerializer(data=new_transaction)
+
+        if transaction_serializer.is_valid():
+            transaction_serializer.save()
+            return Response(transaction_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(transaction_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+
+    # Obtendo todas as transações
+    if request.method == 'GET':
+
+        transactions = Transaction.objects.all()
+        transaction_serializers = TransactionSerializer(transactions, many=True)
+
+        return Response(transaction_serializers.data, status=status.HTTP_200_OK)
+    
+    return Response(status=status.HTTP_400_BAD_REQUEST)
